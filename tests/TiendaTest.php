714b8db84b0caeada8a2d884d8196fa60e7013b1
Adrian Juarez <adrianjj2003@gmail.com>
2025-04-01 13:23:52 +0200

    [refactor] Se ha cambiado el naming de todos los test, ahora pone que hacen y que devuelven. También se ha simplificado el test eliminarArrozDevuelveProductoInexistente

--- tests/TiendaTest.php
+++ tests/TiendaTest.php
@@ -5,37 +5,36 @@ use Deg540\DockerPHPBoilerplate\Tienda;
 class TiendaTest extends TestCase
 {
     /** @test */
-    public function agregarProductoSinCantidad()
+    public function agregarPanSinCantidadDevuelvePanx1()
     {
         $tienda = new Tienda();
         $this->assertEquals("pan x1", $tienda->ejecutar("añadir pan"));
     }
     /** @test */
-    public function agregarProductoConCantidad()
+    public function agregarLecheConCantidad2DevuelveLechex2()
     {
         $tienda = new Tienda();
         $this->assertEquals("leche x2", $tienda->ejecutar("añadir leche 2"));
     }
     /** @test */
-    public function eliminarProductoExistente()
+    public function eliminarLecheExistenteDevuelveNada()
     {
         $tienda = new Tienda();
         $tienda->ejecutar("añadir leche 2");
         $this->assertEquals("", $tienda->ejecutar("eliminar leche"));
     }
     /** @test */
-    public function agregarProductoExistenteConCantidad()
+    public function agregarPanLuegoPan2DevuelvePanx3()
     {
         $tienda = new Tienda();
         $tienda->ejecutar("añadir pan");
         $this->assertEquals("pan x3", $tienda->ejecutar("añadir pan 2"));
     }
     /** @test */
-    public function eliminarProductoInexistente()
+    public function eliminarArrozDevuelveProductoInexistente()
     {
         $tienda = new Tienda();
-        $resultado = $tienda->ejecutar("eliminar arroz");
-        $this->assertEquals("El producto seleccionado no existe", $resultado);
+        $this->assertEquals("El producto seleccionado no existe", $tienda->ejecutar("eliminar arroz"));
     }
 
 }
